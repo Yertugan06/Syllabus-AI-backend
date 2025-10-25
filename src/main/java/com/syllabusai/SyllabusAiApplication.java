69f9797bc2f5bb7220579faff74789e5cc25b7de
Yertugan <yertugan06@gmail.com>
2025-10-25 14:18:29 +0500

    first commit

---
+++ src/main/java/com/syllabusai/SyllabusAiApplication.java
@@ -0,0 +1,13 @@
+package com.syllabusai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyllabusAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyllabusAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syllabusai/SyllabusAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syllabusai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyllabusAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
